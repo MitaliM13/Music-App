ad8b891ef5764cd4f21c9861d89bbff3679753c5
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2024-08-27 15:38:36 +0530

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Player from './Player'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Player/>
   </StrictMode>,
 )
