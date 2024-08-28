dbe8b08b1c1a46d4faa4d2dd789f7022630d0eb8
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2024-08-28 10:31:45 +0530

    Modify

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,8 +1,39 @@
-
+import { useState, useEffect } from "react"
+import spotifyLogo from '../src/assets/Logo.svg'
 
 function Player() {
-  return (
-    <div>Player</div>
+
+    const [song, setSong] = useState([])
+    const url = "https://cms.samespace.com/items/songs"
+
+    useEffect(() => {
+        const fetchSongs = async() => {
+            try {
+                const response = await fetch(url)
+                const data = await response.json()
+                setSong(data)
+            }
+            catch (error){
+                console.error("Error fetching songs",error);
+            }
+        }
+        fetchSongs()
+    }, [])
+
+    return (
+        <>
+            <img src={spotifyLogo} />
+            {song.length > 0 ? (
+                song.map((songs) => (
+                    <div key={songs.id}>
+                        <img src={songs.url} alt={songs.title} />
+                        <p>{songs.title}</p>
+                    </div>
+                ))
+            ) : (
+                <p>Loading...</p>
+            )}
+        </>
   )
 }
 
